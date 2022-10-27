043d43512ab3987aa8f481a7b7373a3b148311c9
AstrikTorres <astriktorres@gmail.com>
2022-10-27 02:06:57 -0500

    Send a message

--- src/index.js
+++ src/index.js
@@ -11,8 +11,16 @@ client.on('qr', (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log('Client is ready!');
+  
+  const number = '+525566271843';
+  const message = 'Hello world!';
+  // To Chat id we have to delete "+" from the beginning and add "@c.us" at the end of the number.
+  const chatId = number.substring(1) + '@c.us';
+
+  const reqMessage = await client.sendMessage(chatId, message);
+  console.log(reqMessage);
 });
 
 client.on('authenticated', () => {
