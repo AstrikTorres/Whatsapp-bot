a7a8415edeef4acd21bff7d56480801bf451527f
AstrikTorres <astriktorres@gmail.com>
2022-10-27 02:34:28 -0500

    Add message scheduler with the package node-schedule

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const client = require('./whatsapp-client');
+const schedule = require('node-schedule');
 
 client.initialize();
 
 client.on('ready', async () => { 
+  console.log('Client is ready!');
+});
+
+const date = new Date(2022, 9, 27, 3, 43, 0);
+
+const scheduledMessage = schedule.scheduleJob(date, async () => {
+  console.log('Sending message');
   const number = '+5215566271843';
   const message = 'Hello world!';
   // To Chat id we have to delete "+" from the beginning and add "@c.us" at the end of the number.
