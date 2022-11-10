ef550883b5f72f62b60fb01e276f3ef0417fb10e
AstrikTorres <astriktorres@gmail.com>
2022-11-10 14:39:35 -0600

    Apply polymorphism to send right away or schedule a message

--- src/services/whatsappScheduled.service.js
+++ src/services/whatsappScheduled.service.js
@@ -2,13 +2,17 @@ require('dotenv').config()
 const client = require('../utils/whatsapp-client');
 const whatsappScheduler = require('../utils/whatsapp-scheduler');
 
-const sendAWhatsapp = async (receiver, message, date) => {
+const sendAWhatsapp = async (receiver, message, date = undefined) => {
   const req = whatsappScheduler(
     client,
     receiver,
     message,
     date,
   );
+
+  if (!date) {
+    return req;
+  }
 
   const whatsapp = {
     receiver,
--- src/utils/whatsapp-scheduler.js
+++ src/utils/whatsapp-scheduler.js
@@ -1,12 +1,26 @@
 const schedule = require('node-schedule');
 
-const whatsappScheduler = (clientWhatsapp, number, message, date) => {
+const scheduledMessage = (clientWhatsapp, number, message, date) => {
   const chatId = number.substring(1) + '@c.us';
 
   const reqMessage = schedule.scheduleJob(date, async () => {
     await clientWhatsapp.sendMessage(chatId, message);
   });
   return reqMessage;
+};
+
+const sendMessage = async (clientWhatsapp, number, message) => {
+  const chatId = number.substring(1) + '@c.us';
+  const messageReq = await clientWhatsapp.sendMessage(chatId, message);
+  return messageReq;
+};
+
+const whatsappScheduler = (...args) => {
+  const [clientWhatsapp, number, message, date] = args;
+  if (date) {
+    return scheduledMessage(clientWhatsapp, number, message, date);
+  }
+  return sendMessage(clientWhatsapp, number, message);
 };
 
 module.exports = whatsappScheduler;
